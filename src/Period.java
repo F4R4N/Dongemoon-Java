4ab16892c523c2f77de7de9785ac9e7a68698332
Faran <farantgh@gmail.com>
2024-02-14 16:24:11 +0330

    add periods constructor and getter setters

--- src/Period.java
+++ src/Period.java
@@ -1,3 +1,52 @@
-public class Period {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Period {
+    private String name;
+    private Date startDate;
+    private ArrayList<Person> persons;
+    private ArrayList<Purchase> purchases;
+    private User owner;
+
+    public Period(String name, Date startDate, User owner) {
+        this.name = name;
+        this.startDate = startDate;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
+
+    public ArrayList<Purchase> getPurchases() {
+        return purchases;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void addPerson(Person person) {
+        this.persons.add(person);
+    }
+
+    public void addPurchase(Purchase purchase) {
+        this.purchases.add(purchase);
+    }
 }
