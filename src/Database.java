32ef7538fdc723ad41e9fbdb139536559f8fa90f
Faran <farantgh@gmail.com>
2024-02-14 16:23:43 +0330

    add datas that app should store

--- src/Database.java
+++ src/Database.java
@@ -1,3 +1,6 @@
-public class Database {
+import java.util.ArrayList;
 
+public class Database {
+    public static ArrayList<User> users = new ArrayList<User>();
+    public static ArrayList<Period> periods = new ArrayList<Period>();
 }
