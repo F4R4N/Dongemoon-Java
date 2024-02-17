3503a3329f112dac635981f313c883c2b98fb93f
Faran <farantgh@gmail.com>
2024-02-17 10:42:36 +0330

    create person coefficient class

--- src/PersonCoefficient.java
+++ src/PersonCoefficient.java
@@ -1,3 +1,39 @@
-public class PersonCoefficient {
+import java.util.ArrayList;
 
+public class PersonCoefficient {
+    private Person person;
+    private int coefficient;
+
+    public PersonCoefficient(Person person, int coefficient) {
+        this.person = person;
+        this.coefficient = coefficient;
+    }
+    public int getCoefficient() {
+        return coefficient;
+    }
+    public Person getPerson() {
+        return person;
+    }
+    public void setCoefficient(int coefficient) {
+        this.coefficient = coefficient;
+    }
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public static void printPersonCoefficients(ArrayList<PersonCoefficient> persons) {
+        if (persons.size()==0) {
+            System.out.println("No person exist yet");
+        } else {
+            String format = "|%-10s  |%-30s|%-10s|%n";
+            System.out.printf(format, "NO.", "Name", "Coefficient");
+            System.out
+                    .print(String.format("|%012d|%030d|%-10s|%n", 0, 0,0)
+                            .replace("0", "-"));
+            for (int index = 0; index < persons.size(); index++) {
+                PersonCoefficient person = persons.get(index);
+                System.out.printf(format, (index + 1), person.getPerson().getName(), person.getCoefficient());
+            }
+        }
+    }
 }
