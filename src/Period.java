83505b2a9c209c7dc9f575ac7b5349a2368d338c
Faran <farantgh@gmail.com>
2024-02-17 10:41:56 +0330

    update period detail and add required method for it.

--- src/Period.java
+++ src/Period.java
@@ -2,6 +2,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Period {
     private String name;
@@ -59,7 +61,44 @@ public class Period {
         }
     }
 
-    public int getTotalExpenses(){
+    public static void printListOfPeriods(ArrayList<Period> periods) {
+        if (periods.size() == 0) {
+            System.out.println("No period exist yet.");
+        } else {
+            String format = "|%-10s  |%-25s|%-35s|%n";
+            System.out.printf(format, "NO.", "Name", "Start Date");
+            System.out
+                    .print(String.format("|%012d|%025d|%035d|%n", 0, 0, 0)
+                            .replace("0", "-"));
+            for (int index = 0; index < periods.size(); index++) {
+                Period period = periods.get(index);
+                System.out.printf(format, (index + 1), period.getName(), period.getStartDate());
+            }
+        }
+    }
+    public static void printPeriods(ArrayList<Period> periods) {
+        if (periods.size() == 0) {
+            System.out.println("No period exist yet.");
+        } else {
+            String format = "|%-10s  |%-25s|%-35s|%n";
+            System.out.printf(format, "NO.", "Name", "Start Date");
+            System.out
+                    .print(String.format("|%012d|%025d|%035d|%n", 0, 0, 0)
+                            .replace("0", "-"));
+            for (int index = 0; index < periods.size(); index++) {
+                Period period = periods.get(index);
+                System.out.printf(format, (index + 1), period.getName(), period.getStartDate());
+
+                Person.printPersons(period.getPersons());
+                UI.printPurchasesInPeriod(period);
+
+                System.out.println(
+                        "---------------------------------------------------------------------------------------------------------");
+            }
+        }
+    }
+
+    public int getTotalExpenses() {
         int expenses = 0;
         for (int index = 0; index < this.purchases.size(); index++) {
             expenses += this.purchases.get(index).getExpense();
@@ -67,11 +106,58 @@ public class Period {
         return expenses;
     }
 
-    public static void printPeriodDetail(Period period){
-        UI.printTitle("'"+period.getName()+"'s' period detail");
-        System.out.println("Start date and time"+dateAndTimeParser.format(period.getStartDate()));
-        System.out.println("Number of persons involved in this period: "+period.persons.size());
-        System.out.println("Total Expenses: "+ period.getTotalExpenses());
-        System.out.println("Number of purchases in period: "+ period.getPurchases().size());
+    public static void printPeriodDetail(Period period) {
+        UI.printTitle("'" + period.getName() + "'s' period detail");
+        System.out.println("Start date and time: " + dateAndTimeParser.format(period.getStartDate()));
+        System.out.println("Number of persons involved in this period: " + period.persons.size());
+        System.out.println("Total Expenses: " + period.getTotalExpenses());
+        System.out.println("Number of purchases in period: " + period.getPurchases().size());
+        System.out.println("Each persons average expense: "+ period.getOverallPersonAverageExpense());
+        HashMap<Person, Integer> personDirectExpenses = period.getPersonsDirectExpenses();
+        printPersonsDirectExpenses(period, personDirectExpenses);
+
+
+    }
+
+    public double getOverallPersonAverageExpense(){
+        int totalExpenses = 0;
+        for (int index = 0; index < this.getPurchases().size(); index++) {
+            totalExpenses += this.getPurchases().get(index).getExpense();
+        }
+        return totalExpenses / this.getPurchases().size();
+    }
+
+    public HashMap<Person, Integer> getPersonsDirectExpenses() {
+        HashMap<Person, Integer> personsAverageExpenses = new HashMap<Person, Integer>();
+        for (int index = 0; index < this.purchases.size(); index++) {
+            Purchase purchase = this.purchases.get(index);
+            personsAverageExpenses.put(purchase.getBuyer(),
+                    personsAverageExpenses.getOrDefault(purchase.getBuyer(), 0) + purchase.getExpense());
+        }
+        return personsAverageExpenses;
+    }
+
+    // TODO: TEST WITH PERSON AN PURCHASES
+    public static void printPersonsDirectExpenses(Period period, HashMap<Person, Integer> personAverageExpenses) {
+        System.out.println("Persons average expenses in '" + period.getName() + "'s' period.");
+        for (Map.Entry<Person, Integer> entry : personAverageExpenses.entrySet()) {
+            Person person = entry.getKey();
+            Integer directExpense = entry.getValue();
+            System.out.println("'" + person.getName() + "' has direct expense of: '" + directExpense + "'");
+        }
+    }
+
+    public static boolean isInvalidIndex(ArrayList<Period> periods, int periodIndexInput) {
+        return periodIndexInput > periods.size() - 1 || periodIndexInput < 0;
+    }
+
+    public static boolean isNameDuplicated(String name) {
+        ArrayList<Period> periods = User.getLoggedInUser().getPeriods();
+        for (int i = 0; i < periods.size(); i++) {
+            if (periods.get(i).getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
